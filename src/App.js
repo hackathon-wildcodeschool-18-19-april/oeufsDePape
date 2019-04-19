63dfb6126601a54ba8679cf44f73f623c3affe2c
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2019-04-19 14:08:04 +0200

    App finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './OeufsDePape.jpg'
+import Game from './Game'
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <img src={logo} alt='Oeufs de Pape'style={{width:'100%'}}/>
+        <Game />
       </div>
     );
   }
